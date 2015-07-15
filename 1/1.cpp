8a33b8defe4395c83d8864d0666dfa9b08c40a65
John Marinelli <jonmarinelli@hotmail.com>
2015-07-15 00:56:27 -0700

    first haskell exercise

--- 1/1.cpp
+++ 1/1.cpp
@@ -1,16 +1,59 @@
 #include <iostream>
+// https://monoinfinito.wordpress.com/series/introduction-to-c-template-metaprogramming/
 
-template <typename T>
-struct myLast {
-  static constexpr T value = ;
+struct NIL {
+  typedef NIL Head;
+  typedef NIL Tail;
+};
+
+template <typename H, typename T = NIL>
+struct List {
+  typedef H Head;
+  typedef T Tail;
+};
+
+template <int v>
+struct Int {
+  static const int Value = v;
+};
+
+template <typename List>
+struct Length {
+  // 'typename' tells compiler that List::Tail is a type, not a variable
+  typedef typename List::Tail tail;
+
+  enum { tailLength = Length<tail>::result };
+  enum { result = tailLength + 1 };
+};
+
+template <>
+struct Length<NIL> {
+  enum { result = 0 };
+};
+
+template <typename H, typename T>
+struct Last {
+  typedef H Head;
+  typedef T Tail;
+
+  enum { Value = Last<Tail::Head, Tail::Tail>::Value };
+};
+
+template <typename H>
+struct Last<H, NIL> {
+  enum { Value = H::Value };
 };
 /*
-template <typename ... Args>
-struct myLast {
-
+template <typename lis>
+struct Last {
+  typedef typename list::Head Head;
+  typedef typename list::Tail Tail;
+  enum { Value = Last<Head, Tail>::Value };
 };
 */
 
 int main(int argc, char* args[]) {
-  //myLast<1, 2, 3, 4>::value;
+  typedef List< Int<1>, List<Int<2>, List<Int<3>> > > list;
+  std::cout << Length<list>::result << '\n';
+  std::cout << Last<list::Head, list::Tail>::Value << '\n';
 }
